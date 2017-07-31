7b1bdfcf774ed530362d8da7efbe8bfbc9951272
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-07-31 13:17:53 +0100

    Updated link names for albums on releases.html

--- js/custom_js/covers_script.js
+++ js/custom_js/covers_script.js
@@ -1,14 +1,14 @@
 COVERS = {
-    "podcast-album": "url(../images/Sample_09.jpg)",
-    "first-album": "url(../images/covers/satellite.png)",
-    "second-album": "url(../images/covers/mindmap.png)",
-    "third-album": "url(../images/covers/nihilus1.png)",
+    "podcast-album": "url(images/Sample_09.jpg)",
+    "first-album": "url(images/covers/satellite.png)",
+    "second-album": "url(images/covers/mindmap.png)",
+    "third-album": "url(images/covers/nihilus1.png)",
 }
 LOGOS = {
-    "podcast-album": "url(../images/factorybg1.jpg)",
-    "first-album": "url(../images/logos/deviateLogo1.jpg)",
-    "second-album": "url(../images/logos/andor.png)",
-    "third-album": "url(../images/logos/simonPlaceholder.png)",
+    "podcast-album": "url(images/factorybg1.jpg)",
+    "first-album": "url(images/logos/deviateLogo1.jpg)",
+    "second-album": "url(images/logos/andor.png)",
+    "third-album": "url(images/logos/simonPlaceholder.png)",
 }
 
 function showAlbum(albumName) {
