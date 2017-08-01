3ba1301b347cc218805e0e95f6baec999dd5c876
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-08-01 19:42:21 +0100

    Added minor style changes to all pages and homepage animation

--- js/custom_js/covers_script.js
+++ js/custom_js/covers_script.js
@@ -1,13 +1,13 @@
 COVERS = {
     "podcast-album": "url(images/Sample_09.jpg)",
-    "first-album": "url(images/covers/satellite.png)",
-    "second-album": "url(images/covers/mindmap.png)",
+    "first-album": "url(images/covers/mindmap.png)",
+    "second-album": "url(images/covers/satellite.png)",
     "third-album": "url(images/covers/nihilus1.png)",
 }
 LOGOS = {
     "podcast-album": "url(images/factorybg1.jpg)",
-    "first-album": "url(images/logos/deviateLogo1.jpg)",
-    "second-album": "url(images/logos/andor.png)",
+    "first-album": "url(images/logos/andor.png)",
+    "second-album": "url(images/logos/deviateLogo1.jpg)",
     "third-album": "url(images/logos/simonPlaceholder.png)",
 }
 
--- js/custom_js/script.js
+++ js/custom_js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     /* logo box */
     $("#home-logo-box").mouseenter(function() {
+        $("#listenbtn").animate({fontSize: '1.5em'} ,"slow");
         $("#home-logo").fadeTo(1000, 1);
         $("#listenbtn").fadeTo(1000, 1);
         $("#jumbo-text-box").fadeTo(1000, 0.3);
@@ -9,14 +10,14 @@ $(document).ready(function(){
 
     });
     $('#home-logo-box').mouseleave(function() {
+        $("#listenbtn").animate({fontSize: '0em'} ,"slow");
         $("#home-logo").fadeTo(1000, 0.5);
         $("#listenbtn").fadeTo(1000, 0.5);
         $("#jumbo-text-box").fadeTo(800, 0.5);
-        /*$("#home-logo").addClass("blur");*/
-        $("#listenbtn").addClass("visibility");
     });
     /* text box */
     $("#jumbo-text-box").mouseenter(function(){
+        $("#moreinfo").animate({fontSize: '1.5em'} ,"slow");
         $("#jumbo-text-box").fadeTo(1000, 1);
         $("#moreinfo").fadeTo(1000, 1);
         $("#home-logo").fadeTo(1000, 0.3);
@@ -25,28 +26,11 @@ $(document).ready(function(){
         /*$("#jumbo-text-box").addClass("white-box");*/
     });
     $("#jumbo-text-box").mouseleave(function(){
+        $("#moreinfo").animate({fontSize: '0em'} ,"slow");
         $("#jumbo-text-box").fadeTo(800, 0.5);
         $("#moreinfo").fadeTo(800, 0.5);
-        $("#moreinfo").addClass("visibility");
-        /*$("#jumbo-text-box").removeClass("white-box");*/
     });
 
-/* audio player */
-    $("#box1").click(function(){
-        $("audio").attr("src","/audio/theboxPart1.mp3");
-        $("#audiojs_wrapper0").removeClass("playing");
-    });
-    $("#box2").click(function(){
-        $("audio").attr("src","/audio/theboxPart2.mp3");
-        $("#audiojs_wrapper0").removeClass("playing");
-    });
-    $("#levee").click(function(){
-        $("audio").attr("src","/audio/leveebreaks.mp3");
-        $("#audiojs_wrapper0").removeClass("playing");
-    });
-    $("#goodbad").click(function(){
-        $("audio").attr("src","/audio/goodbadugly.mp3");
-        $("#audiojs_wrapper0").removeClass("playing");
-    });
+
 
 });
