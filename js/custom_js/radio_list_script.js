adeefa0930ec0c8b06ec20df80ed99f4ff80bf34
Paul M Fleming <paul.stillbreathing@gmail.com>
2017-10-02 14:09:19 +0100

    updated releses radio list script

--- js/custom_js/radio_list_script.js
+++ js/custom_js/radio_list_script.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
     $("#radio-list-item-one").mouseleave(function() {
         $("#first-album.album-button").css('text-decoration', 'none');
     });
+
+    $("#radio-list-item-two").mouseenter(function() {
+        $("#second-album.album-button").css('text-decoration', 'underline');
+    });
+    $("#radio-list-item-two").mouseleave(function() {
+        $("#second-album.album-button").css('text-decoration', 'none');
+    });
+
+    $("#radio-list-item-three").mouseenter(function() {
+        $("#third-album.album-button").css('text-decoration', 'underline');
+    });
+    $("#radio-list-item-three").mouseleave(function() {
+        $("#third-album.album-button").css('text-decoration', 'none');
+    });
 });
